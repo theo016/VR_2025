fc48928b1367fb22ed5fec5facb6b78792d996ec
apostolouagge <78177328+theo016@users.noreply.github.com>
2025-02-01 19:51:09 +0200

    tjyf

---
+++ practice/Assets/MoveAxis.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAxis : MonoBehaviour
+{
+    float speed = 0.5f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        //print("Hello World! on start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.position + new Vector3(0, 0, speed * Time.deltaTime);
+        //Debug.Log("Hello World! on update");
+    }
+}
